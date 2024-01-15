5e2fd49d31547f186a9c86fda6f0f2e1290174eb
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-15 10:53:46 +0530

    Manangment Data Modelling

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express = require('express')
+// require('dotenv').config()
 
-const app = express()
+// const express = require('express')
 
-const PORT = 8000;
+// const app = express()
 
-app.get('/', (req,res)=>{
-    res.send('Hello Homepage');
-})
-app.get('/login', (req,res)=>{
-    res.send(' <h1> Hello From Login </h1> ')
-})
+// const PORT = 8000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server started at Port ${PORT}`);
-})
+// app.get('/', (req,res)=>{
+//     res.send('Hello Homepage');
+// })
+// app.get('/login', (req,res)=>{
+//     res.send(' <h1> Hello From Login </h1> ')
+// })
+
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`Server started at Port ${PORT}`);
+// })
+
+
+
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.send(console.log("Hello server"))
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
